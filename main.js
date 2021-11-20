7f648d38122938aadd573576e00077cbd1752982
SeriouslyWhatAmI <80988146+SeriouslyWhatAmI@users.noreply.github.com>
2021-11-19 19:32:20 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition;
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function run (event) {
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    if(Content == "Take my selfie.") {
+        console.log("Taking selfie in 5 seconds");
+    }
+    speak();
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data = "Taking your selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width:320,
+    height:250,
+    image_format:'png',
+    png_quality:90
+})
+camera = document.getElementById("camera");
